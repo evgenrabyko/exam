28b5e37e8e2854a1be76ca3fd7b64d818d368b1e
evgen <evgen@gmail.com>
2020-11-19 18:56:44 +0300

    hw_1

---
+++ js/script.js
@@ -0,0 +1,74 @@
+//2 задание
+
+let a1 = 5 % 3;
+let a2 = 3 % 5;
+let a3 = 5 + '3'; 
+let a4 = '5' - 3;
+let a5 = 75 + 'кг';
+let a6 = 785 * 653;
+let a7 = 100 / 20;
+let a8 = 0 * 0;
+let a9 = 0 / 2;
+let a10 = 89 / 0;
+let a11 = 98 + 2;
+let a12 = 5 - 98;
+let a13 = (8 + 56 * 4) / 5;
+let a14 = (9 - 12) * 7 / (5 + 2);
+let a15 = + "123";
+let a16 = 1 || 0;
+let a17 = false || true;
+let a18 = true > 0;
+
+console.log(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18);
+console.log(typeof a1, typeof a2, typeof a3, typeof a4, typeof a5, typeof a6, typeof a7, typeof a8, typeof a9, typeof a10, typeof a11, typeof a12, typeof a13, typeof a14, typeof a15, typeof a16, typeof a17, typeof a18);
+
+//3 задание
+
+let wei = 23
+    hei = 10
+    SPryam = wei * hei
+    console.log(SPryam + " см.куб. ");
+
+//4 задание 
+
+let h = 10 
+    VCilindra = a7 * h
+console.log(VCilindra + " см.кв. ");
+
+//5 задание 
+
+let Pi = 3.14
+    r = 5
+    SKruga = Pi * r ** 2
+    console.log(SKruga + " см.кв. ");
+
+//6 задание
+
+let a = 5
+    b = 7
+    h
+    STrap = (a + b)/2 * h
+    console.log(STrap + " см.кв. ");
+
+//7 задание
+
+let S = 2e6
+    p = 0.01
+    y = 5
+    Pereplata = S * ((p * (1 + p) ** y) / ((1 + p)**y - 1))    
+    console.log(Pereplata.toFixed(0) + " руб ")
+
+//8 задание
+
+let a_ = 8
+    b_ = 3
+    x1 = (16 - a_ + b_) / 2
+    x2 = (a_ + 15 * b_) / (b_ - 6)
+    x3 = 23780 / (1 + 2 + a_ + b_)
+    console.log(x1.toFixed(1))
+    console.log(x2.toFixed(2))
+    console.log(x3.toFixed(2))
+
+//9 задание 
+
+
